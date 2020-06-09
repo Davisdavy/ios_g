5ca124335baf9b9636a9cab30afe4abe865cd85b
Davisdavy <davisdavy96@gmail.com>
2020-06-09 20:43:16 +0300

    Reduc holder size

--- lib/holder.dart
+++ lib/holder.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:goga/account.dart';
-
 import 'package:goga/categories.dart';
 import 'package:goga/help.dart';
 import 'package:goga/home.dart';
